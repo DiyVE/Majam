1d80519bb9af6c20135d0d1022d20fe853018e78
DiyVE <ramirez.clement3@gmail.com>
2023-07-09 16:11:57 +0200

    Add animation to background and some buttons/Textlabel

--- app/src/main/java/com/example/majam/DummyActivity.kt
+++ app/src/main/java/com/example/majam/DummyActivity.kt
@@ -7,5 +7,6 @@ class DummyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dummy)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     }
 }
--- app/src/main/java/com/example/majam/MainActivity.kt
+++ app/src/main/java/com/example/majam/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.majam
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.majam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +16,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
+        val animationDrawable = binding.root.background as AnimationDrawable
+        val clickAnim: Animation = AnimationUtils.loadAnimation(this, R.anim.little_bounce)
+
+        animationDrawable.setEnterFadeDuration(10)
+        animationDrawable.setExitFadeDuration(1000)
+        animationDrawable.start()
+
+        binding.playButton.setOnClickListener {
             val changePage = Intent(this, DummyActivity::class.java)
             startActivity(changePage)
+            binding.playButton.startAnimation(clickAnim)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+
+        binding.settingsButton.setOnClickListener{
+            binding.settingsButton.startAnimation(clickAnim)
         }
     }
 }
