1e74238435436e01f03c30364cc87fec5b2bbb92
DiyVE <ramirez.clement3@gmail.com>
2023-07-09 12:59:10 +0200

    Add a Dummy Activity and set the app to fullscreen

--- app/src/main/java/com/example/majam/MainActivity.kt
+++ app/src/main/java/com/example/majam/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.majam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.majam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val changePage = Intent(this, DummyActivity::class.java)
+            startActivity(changePage)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
     }
 }
